691720666890487e40618b545fdac2b0c995acfe
haruna <news.sigeak@gmail.com>
2025-07-20 03:03:08 +0900

    フォームで入力した値が表示されるようにするため編集

--- src/resources/views/confirm.blade.php
+++ src/resources/views/confirm.blade.php
@@ -6,7 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Contact Form</title>
+    {{-- css/sanitize.cssのリンクを読み込んで使う --}}
     <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
+    {{-- css/confirmのリンクを読み込んで使う --}}
     <link rel="stylesheet" href="{{ asset('css/confirm.css') }}" />
 </head>
 
@@ -20,6 +22,7 @@
     </header>
 
     <main>
+        {{-- <?php print_r($contact['name']) ?> --}}
         <div class="confirm__content">
             <div class="confirm__heading">
                 <h2>お問い合わせ内容確認</h2>
@@ -30,25 +33,29 @@
                         <tr class="confirm-table__row">
                             <th class="confirm-table__header">お名前</th>
                             <td class="confirm-table__text">
-                                <input type="text" name="name" value="サンプルテキスト" />
+                                {{-- <input type="text" name="name" value="サンプルテキスト" /> --}}
+                                <input type="text" name="name" value="{{ $contact['name'] }}" readonly />
                             </td>
                         </tr>
                         <tr class="confirm-table__row">
                             <th class="confirm-table__header">メールアドレス</th>
                             <td class="confirm-table__text">
-                                <input type="email" name="email" value="サンプルテキスト">
+                                {{-- <input type="email" name="email" value="サンプルテキスト"> --}}
+                                <input type="email" name="email" value="{{ $contact['email'] }}" readonly />
                             </td>
                         </tr>
                         <tr class="confirm-table__row">
                             <th class="confirm-table__header">電話番号</th>
                             <td class="confirm-table__text">
-                                <input type="tel" name="tel" value="サンプルテキスト" />
+                                {{-- <input type="tel" name="tel" value="サンプルテキスト" /> --}}
+                                <input type="tel" name="tel" value="{{ $contact['tel'] }}" readonly />
                             </td>
                         </tr>
                         <tr class="confirm-table__row">
                             <th class="confirm-table__header">お問い合わせ内容</th>
                             <td class="confirm-table__text">
-                                <input type="text" name="content" value="サンプルテキスト" />
+                                {{-- <input type="text" name="content" value="サンプルテキスト" /> --}}
+                                <input type="text" name="content" value="{{ $contact['content'] }}" readonly />
                             </td>
                         </tr>
                     </table>
